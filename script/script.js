c45b9ec4ef3d11e24cd3fe6afebf2c7a2070c1be
FLKLeslie <166480735+FLKLeslie@users.noreply.github.com>
2025-03-17 17:03:46 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let weatherDatabase = [
+    {
+     "Yaounde" :  [["23","rainy"],["21","rainy"],["20","drizzling"],["27","sunny"],["30","sunny"],["22","rainy"]]
+    },
+    {
+     "Douala" :  [["24","rainy"],["21","sunny"],["20","drizzling"],["17","drizzling"],["30","sunny"],["29","sunny"]]
+    },
+    {
+     "Bamenda" :  [["30","sunny"],["21","rainy"],["20","Thunderstorm"],["34","sunny"],["30","sunny"],["22","Windy"]]
+    },
+    {
+     "Buea" :  [["20","rainy"],["22","sunny"],["20","drizzling"],["27","Thunderstorm"],["30","sunny"],["21","sunny"]]
+    },
+    {
+     "Bafoussam" :  [["22","windy"],["21","rainy"],["20","drizzling"],["27","sunny"],["30","sunny"],["22","rainy"]]
+    }
+]
+
